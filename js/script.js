3268a9719114743b5f9a77667199673b1089f180
N-akademy <n.wachani@outlook.fr>
2019-10-24 10:09:06 +0200

    second commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+// document.querySelector("#title").addEventListener("click",function(){
+//     console.log("Mon Eventlistener a fonctionné!")
+// })
+
+
+
+// document.querySelector("#title").addEventListener("click", () => {
+//     document.querySelector("#title").classList.add("#modified-title");
+// })
+
+// let button = document.querySelector("button");
+// button.addEventListener("click", () => {
+//     console.log(document.querySelector("input").value);
+// });
+    
+// document.querySelector("text").value
+
+
+// document.getElementById("#onclick").addEventListener("click",function(){
+//     document.getElementById("body").style.color = "blue"
+// });
+
+
+
+
+
+
+
+
